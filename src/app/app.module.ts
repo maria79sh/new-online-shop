366fb054ce7c6b48dcc9e80ef6b5f8de19810e5b
Reza <r.shakeri9210@gmail.com>
2020-12-10 11:20:42 +0330

    fixing store and layout problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { UserModule } from './user/user.module';
-import { StoreModule } from './store/store.module';
-import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
--- src/app/layout/layout.routing.ts
+++ src/app/layout/layout.routing.ts
@@ -1,24 +1,25 @@
+import { LayoutComponent } from './components/layout/layout.component';
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  // {
-  //   path: "",
-  //   component: LayoutComponent,
-  //   children: [
-  //     { path: "", redirectTo: "/admin", pathMatch: "full" },
-  //     {
-  //       path: "admin",
-  //       loadChildren: () =>
-  //         import("../admin/admin.module").then((m) => m.AdminModule),
-  //     },
-  //     {
-  //       path: "seller",
-  //       loadChildren: () =>
-  //         import("../seller/seller.module").then((m) => m.SellerModule),
-  //     },
-  //   ],
-  // },
+  {
+    path: "",
+    component: LayoutComponent,
+    children: [
+      // { path: "", redirectTo: "/admin", pathMatch: "full" },
+      // {
+      //   path: "admin",
+      //   loadChildren: () =>
+      //     import("../admin/admin.module").then((m) => m.AdminModule),
+      // },
+      // {
+      //   path: "seller",
+      //   loadChildren: () =>
+      //     import("../seller/seller.module").then((m) => m.SellerModule),
+      // },
+    ],
+  },
 ];
 
 @NgModule({
